fb596fe7bf2e806d4d628102d79d005fd9570b61
jaehyunpyun <pyunjaehyun@gmail.com>
2026-05-25 20:59:42 +0900

    bash_operator dag 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,22 @@
+import pendulum
+
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG, chain
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * * ",
+    start_date=pendulum.datetime(2023, 3, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_commands="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_commands="echo #HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
